9aa1f175bae4fb51a622a3235f3491cb8d865d10
Victor Metodiev <victor@grumspot.com>
2024-12-06 16:52:34 +0200

    Add functionality to get user information and to clear the form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,85 @@
-// Code goes here!
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+
+    return adjDescriptor;
+}
+
+// ProjectInput Class
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputELement: HTMLInputElement;
+    descriptionInputELement: HTMLInputElement;
+    peopleInputELement: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputELement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputELement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputELement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
+        this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputELement.value;
+        const enteredDescription = this.descriptionInputELement.value;
+        const enteredPeople = this.peopleInputELement.value;
+
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputELement.value = '';
+        this.descriptionInputELement.value = '';
+        this.peopleInputELement.value = '';
+    }
+
+    @AutoBind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+
+        const userInput = this.gatherUserInput();
+
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+
+            console.log(title, desc, people);           
+            this.clearInputs();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();
